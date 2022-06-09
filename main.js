b9f5d48ce1c4fd7eab8b13762f86333bdbd91be9
marllondallas <marllon_dallas@hotmail.com>
2022-06-08 22:51:49 -0300

    desafio 1 do Vai Na Web

---
+++ main.js
@@ -0,0 +1,21 @@
+// Crie uma variável que receba um nome;
+var nome = "Marllon";
+
+// Crie uma variável que receba um número;
+var numero = 13;
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+var anoAtual = 2022;
+console.log( anoAtual-numero);
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(numero + anoAtual);
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log");
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+var quartaFeira = 2.5;
+console.log(quartaFeira == Number);
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira);
